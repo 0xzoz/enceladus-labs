c573f5492fb0b81bc588dbd9503f1b0626b3522b
zoz <zeroexzoz@gmail.com>
2023-11-16 10:56:06 +0300

    Align header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,15 +24,25 @@
       height: 100%;
     }
   
+    .center-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 10; /* Place it above the iframe */
+    }
+
     .title {
-      position: absolute;
-      top: 10px;
-      left: 10px;
-      font-family: 'Urbanist', sans-serif;
-      font-weight: 100;
-      font-size: 24px;
-      color: white; /* or any color you prefer */
-      z-index: 10; /* ensures the title is above the iframe */
+        font-family: 'Urbanist', sans-serif;
+        font-weight: 100;
+        font-size: 24px;
+        color: white; /* or any color you prefer */
+        margin-bottom: 20px; /* Adjust the space between the title and the button */
     }
 
     .apply-button {
@@ -69,8 +79,10 @@
     <iframe title="Enceladus" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/65f5f87d930c4f35a4039656c266272a/embed?autospin=1&autostart=1&preload=1&ui_infos=0&ui_stop=0&ui_inspector=0&ui_watermark_link=0&ui_watermark=0&ui_hint=0&ui_ar=0&ui_help=0&ui_settings=0&ui_vr=0&ui_annotations=0"></iframe>
   </div>
   <div class="title">0L Moonshot Program</div>
-  <button class="apply-button">Apply</button>
-  <img src="logo.svg" alt="Enceladus Labs Inc Logo" class="bottom-logo">
+  <div class="center-container">
+    <div class="title">0L Moonshot Program</div>
+    <button class="apply-button">Apply</button>
+  </div>
 
 
   
