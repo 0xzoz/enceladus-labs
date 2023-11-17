f58813a8281598333deea07c45f7ffa5fa70315d
zoz <zeroexzoz@gmail.com>
2023-11-17 00:48:43 -0800

    add form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -79,7 +79,7 @@ iframe {
   </div>
   <div class="top-container">
     <div class="title">0L Moonshot Program</div>
-    <button class="apply-button">Apply</button>
+    <a href="https://enceladus.deform.cc/apply"><button class="apply-button"  >Apply</button></a>
   </div>
   <img src="logo.svg" alt="Enceladus Labs Inc Logo" class="bottom-logo">
 
